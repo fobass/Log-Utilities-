f28f61eca74450fbeaa93d78f8442ad3eb534de5
fobass <isabekove@gmail.com>
2019-08-12 21:50:14 +0800

    table resize

--- view.js
+++ view.js
@@ -7,6 +7,7 @@ require('datatables.net')();
 let fs = require('fs')
 let filename = '2018-11-13_Ticket.log'
 let sno = 0
+let dataSet = []
 
  $(document).ready( ()=> {
     $(document).on('click', '.sidebar-menu .nav-link', (e)=> {
@@ -23,52 +24,19 @@ let sno = 0
             if (files) {
                 console.log(files);
                 document.getElementById('file-path').value = files
+                filename = files[0]
+                loadAndDisplayContacts()
+                prepareTable()
             }
             // event.sender.send('file-path, files')
             })
-    }); 
-
-   
-
-    $(document).on('click', '#load-table', ()=>{
-                $('#dtHorizontalExample').DataTable({
-                    "scrollX": true,
-                    "scrollY": "70vh",
-                    "scrollCollapse": true
-                });
-                $('.dataTables_length').addClass('bs-select');
-    })
+    });
 
 });
 
 ipc.on('file-path', function (event, path) {
     document.getElementById('file-path').innerHTML = path
 })
-
-
-// $(document).on('click', ()=> {
-//     // console.log("My clicked")
-//     setActiveNavItem(1)
-//   });
-
-//   function setActiveNavItem(item){
-//     var navitem = document.getElementById('order_id')
-//     // navitem.parent().addClass("active");
-//     if (navitem.parentNode.className == "active" || navitem.parentNode.className == "active open" ) {
-
-//         // navitem.parentNode.className = "active";
-//         console.log(navitem.className)
-//         $(navitem).parent().addClass('active').siblings().removeClass('active');
-
-//     } 
-//   }
-
-// selectDirBtn.addEventListener('click', function (event) {
-//     ipc.send('open-directory-dialog')
-// })
-
-
-
 
 window.addEventListener('contextmenu', (event)=>{
     event.preventDefault()
@@ -89,38 +57,43 @@ $('#sendIPCAsync').on('click', ()=> {
     document.getElementById('respICPAsync').innerHTML = msg
     
 })
-$('#load-to1').on('click', ()=>{
-    console.log("test")
-})
-
-$(document).ready( function() {
-    $("#load-to").on("click", function() {
-        console.log("Button Clicked")
-    });
-});
-
-
-
-$('#test').on('click', () => {
-    // loadAndDisplayContacts()
-    console.log("Button Clicked")
-    // let filePath =  $('#filePath').val()
-    // console.log(filePath)
-    // let logfile = fs.readFile(filePath, (err, data)=>{
-    //     if (err) {
-    //         return console.log(err);
-    //       }
-    //       console.log("Asynchronous read: " + data.toString());
-    //       addEntry(logfile)
-    // })  
-})
 
 function addEntry(LogDt, SourceID, OrderDateTime, TicketNo) {
     if (LogDt && SourceID && OrderDateTime && TicketNo) {
         sno++
         let updateString = '<tr><td>' + LogDt + '</td><td>' + SourceID + '</td><td>' +
             OrderDateTime + '</td></tr>' + TicketNo + '</td></tr>'
-        $('#log-table').append(updateString)
+        $('#data-table').append(updateString)
+    }
+}
+
+function prepareTable(){
+    $(document).ready(function () {
+        $('#data-table').DataTable({
+           "scrollX": true,
+            "scrollY": "70vh",
+            "scrollCollapse": true,
+            data : dataSet,
+            columns: [
+                { title: 'LogDt' },
+                { title: 'SourceID' },
+                { title: 'OrderDateTime'},
+                { title: 'TicketNo'},
+                { title: 'TicketNo'},
+                { title: 'TicketNo'},
+            ]
+        });
+    $('.dataTables_length').addClass('bs-select');
+    });
+}
+
+function updateTableData(data){
+    if (data){
+        data.forEach((ticketLog, index) => {
+            let [LogDt, Space, SourceID, OrderDateTime, TicketNo, m, s] = ticketLog.split('|')
+            let v = [LogDt, SourceID, OrderDateTime, TicketNo, m, s]
+            dataSet.push(v)
+        })
     }
 }
 
@@ -129,11 +102,11 @@ function loadAndDisplayContacts() {
     //Check if file exists
     if (fs.existsSync(filename)) {
         let data = fs.readFileSync(filename, 'utf8').split('\n')
-
-        data.forEach((ticketLog, index) => {
-            let [LogDt, SourceID, OrderDateTime, TicketNo] = ticketLog.split('|')
-            addEntry(LogDt, SourceID, OrderDateTime, TicketNo)
-        })
+        updateTableData(data)
+        // data.forEach((ticketLog, index) => {
+        //     let [LogDt, Space, SourceID, OrderDateTime, TicketNo] = ticketLog.split('|')
+        //     addEntry(LogDt, SourceID, OrderDateTime, TicketNo)
+        // })
 
     } else {
         console.log("File Doesn\'t Exist. Creating new file.")
